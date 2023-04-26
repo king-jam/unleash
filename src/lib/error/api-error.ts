8ddb2409bc39c68471ec3b02f1d62e1b0636bfed
Thomas Heartman <thomas@getunleash.io>
2023-04-26 16:59:01 +0200

    Fix: doesn't double punctuation

--- src/lib/error/api-error.ts
+++ src/lib/error/api-error.ts
@@ -182,7 +182,7 @@ export class UnleashError extends Error {
     }
 
     toString(): string {
-        return `${this.name}: ${this.message}.`;
+        return `${this.name}: ${this.message}`;
     }
 }
 
